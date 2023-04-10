a6feaa7df4f8f5b53eeecbb17b742fdc94adb35b
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-04-10 11:48:06 +0300

    optimization of category with memo and add fake server

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import React from "react";
 import axios from "axios";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { setPizzas } from "./redux/actions/pizzas";
 
+function App() {
+  const dispatch = useDispatch();
 
-// function App() {
-
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({data}) => {
-//       setPizzas(data.pizzas);
-//     })
-//   }, []);
-
-
-//   return 
-// }
-
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({data}) => {
-      this.props.setPizzas(data.pizzas);
+  React.useEffect(() => {
+    // Перенести в Redux  и подключить redux-thunk
+    axios.get('http://localhost:3001/pizzas').then(({data}) => {
+      dispatch(setPizzas(data));
     })
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-        <Route exact path="/" render={() => <Home items={this.props.items} />} />
+  }, [])
+ 
+  return (
+     <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route exact path="/" component={Home} />
         <Route exact path="/cart" component={Cart} />
-        </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items
-  }
-}
+export default App;
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzasAction(items))
-    }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
