6977bcc29e190c2afc3de49b6b26bd74bd068b12
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-04-14 14:00:34 +0300

    refactor code

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import classNames from 'classnames';
-import ContentLoader from "react-content-loader"
-
+import LoadingBlock from './LoadingBlock';
 
 function PizzaBlock({name, imageUrl, price, types, sizes }) {
     const availableTypes = [ 'тонкое', 'традиционное']
     const availableSizes = [26, 30, 40 ]
+
     const [activeType, setActiveType] = React.useState(types[0])
     const [activeSize, setActiveSize] = React.useState(sizes[0])
 
@@ -17,24 +17,7 @@ function PizzaBlock({name, imageUrl, price, types, sizes }) {
         setActiveSize(index);
     }
 
-    return (
-      <ContentLoader 
-      speed={2}
-      width={280}
-      height={460}
-      viewBox="0 0 280 460"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#ecebeb"
-    >
-      <circle cx="119" cy="115" r="111" /> 
-      <rect x="9" y="241" rx="6" ry="6" width="224" height="24" /> 
-      <rect x="180" y="245" rx="0" ry="0" width="0" height="8" /> 
-      <rect x="12" y="281" rx="6" ry="6" width="224" height="81" /> 
-      <rect x="18" y="383" rx="6" ry="6" width="71" height="29" /> 
-      <rect x="83" y="392" rx="0" ry="0" width="6" height="5" /> 
-      <rect x="116" y="374" rx="22" ry="22" width="121" height="42" />
-    </ContentLoader>
-    );
+  
 
     return (
         <div className="pizza-block">
@@ -94,14 +77,14 @@ PizzaBlock.propTypes = {
     imageUrl: PropTypes.string,
     price: PropTypes.number,
     types: PropTypes.arrayOf(PropTypes.number),
-    sizes: PropTypes.arrayOf(PropTypes.number)
+    sizes: PropTypes.arrayOf(PropTypes.number),
 };
 
 PizzaBlock.defaultProps = {
     name: '----',
     price: 0,
     types: [],
-    sizes: []
+    sizes: [],
 };
 
 export default PizzaBlock;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,50 @@
-import React from 'react';
-import { Categories, SortPopup, PizzaBlock} from "../components";
+import React from "react";
+import { Categories, PizzaLoadingBlock, SortPopup } from "../components";
 import { useSelector, useDispatch } from "react-redux";
-import { setCategory } from "../redux/actions/filters"
+import { setCategory } from "../redux/actions/filters";
 import { fetchPizzas } from "../redux/actions/pizzas";
+import PizzaBlock from "../components/PizzaBlock";
 
-
-const categoryNames = [ 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые' ];
+const categoryNames = [
+  "Мясные",
+  "Вегетарианская",
+  "Гриль",
+  "Острые",
+  "Закрытые",
+];
 const sortItems = [
-  {name: 'популярности', type: 'popular'},
-  {name: 'цене', type: 'price'}, 
-  {name: 'алфавиту', type: 'alphabet'}, 
-]
+  { name: "популярности", type: "popular" },
+  { name: "цене", type: "price" },
+  { name: "алфавиту", type: "alphabet" },
+];
 
 function Home() {
   const dispatch = useDispatch();
-  const items = useSelector(({pizzas}) => pizzas.items);
+  const items = useSelector(({ pizzas }) => pizzas.items);
+  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
 
   React.useEffect(() => {
     dispatch(fetchPizzas());
-  }, [])
-
-  const onSelectCategory = React.useCallback((index) => {
-    dispatch(setCategory(index))
   }, []);
 
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
 
-    return (
-        <div className="container">
-          <div className="content__top">
-            <Categories  
-              onClickItem={onSelectCategory}
-              items={categoryNames}/>
-          <SortPopup items={sortItems} />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-                items && items.map(
-                    obj => <PizzaBlock key={obj.id} {...obj} />)
-            }
-          </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories onClickItem={onSelectCategory} items={categoryNames} />
+        <SortPopup items={sortItems} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoaded 
+          ? items.map((obj) => <PizzaBlock key={obj.id} isLoading={true} {...obj} />)
+          : Array(12).fill(<PizzaLoadingBlock />) } 
+      </div>
+    </div>
+  );
 }
 
 export default Home;
