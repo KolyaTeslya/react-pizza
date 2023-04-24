373d645c7eedb3e44e6acac7152d8ca53fe90986
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-04-24 10:29:23 +0300

    finish cart add adding itemPizza

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -4,7 +4,22 @@ const initialState = {
   totalCount: 0,
 };
 
-const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum , 0);
+const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum , 0);
+
+const _get = (obj, path) => {
+  const [firstKey, ...keys] = path.split('.');
+  return keys.reduce((val, key) => {
+    return val[key];
+  }, obj[firstKey])
+}
+
+const getTotaSum = (obj, path) => {
+  return Object.values(obj).reduce((sum, obj) => {
+    const value = _get(obj, path);
+    return sum + value;
+  }, 0);
+};
+
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
@@ -18,17 +33,21 @@ const cart = (state = initialState, action) => {
           [action.payload.id]: {
             items:  currentPizzaItems,
             totalPrice: getTotalPrice(currentPizzaItems), 
-          } ,
+          },
         };
 
-        const items = Object.values(newItems).map((obj) => obj.items);
-        const allPizzas =[].concat.apply([], items);
-        const totalPrice = getTotalPrice(allPizzas);
+        // const totalCount = Object.keys(newItems).reduce(
+        //       (sum, key) => newItems[key].items.length + sum, 0);
+        // const totalPrice = Object.keys(newItems).reduce(
+        //   (sum, key) => newItems[key].totalPrice + sum, 0);
+
+          const totalCount = getTotaSum(newItems, 'items.length');
+          const totalPrice = getTotaSum(newItems, 'totalPrice');
 
         return {
             ...state,
             items: newItems,
-            totalCount: allPizzas.length,
+            totalCount,
             totalPrice,
       };
     }
@@ -47,6 +66,52 @@ const cart = (state = initialState, action) => {
         totalCount: state.totalCount - currentTotalCount,
        };
     }
+
+    case 'PLUS_CART_ITEM':{
+      const newObjItems = [
+        ...state.items[action.payload].items,
+        state.items[action.payload].items[0]
+      ];
+
+      const newItems = {
+        ...state.items,
+        [action.payload]: {
+          items: newObjItems,
+          totalPrice: getTotalPrice(newObjItems), 
+        },
+      };
+      const totalCount = getTotaSum(newItems, 'items.length');
+      const totalPrice = getTotaSum(newItems, 'totalPrice');
+
+      return { 
+        ...state,
+        items: newItems,
+        totalCount,
+        totalPrice,
+      }
+    };
+
+    case 'MINUS_CART_ITEM':{
+      const oldItems = state.items[action.payload].items;
+      const newObjItems = oldItems.length > 1 ? state.items[action.payload].items.slice(1) : oldItems;
+      const newItems = {
+        ...state.items,
+        [action.payload]: {
+          items: newObjItems,
+          totalPrice: getTotalPrice(newObjItems), 
+        },
+      };
+
+      const totalCount = getTotaSum(newItems, 'items.length');
+      const totalPrice = getTotaSum(newItems, 'totalPrice');
+
+      return { 
+        ...state,
+        items: newItems,
+        totalCount,
+        totalPrice,
+      }
+    };
 
     case 'CLEAR_CART':
       return { totalPrice: 0, totalCount: 0, items: {} };
